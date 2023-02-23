699eef622bc0e593437bf4e8711d9a9b7ce51b0b
raven <zlat.dominic.francisc@gmail.com>
2023-02-23 14:21:34 +0200

    test env

--- api/dbop.js
+++ api/dbop.js
@@ -1,1 +1,15 @@
-xx
+require('dotenv').config()
+const cors = require('micro-cors')({ allowMethods: ['GET', 'POST'], origin: '*' });
+const { v4 } = require('uuid');
+const bcrypt = require('bcrypt')
+const mysql = require('mysql2')
+
+function handler(req, res) {
+    if(process.env.PLANETSCALE_DB_USERNAME != undefined){
+        res.json({DAX: 'got it'})
+    }else{
+        res.json({DAX: 'not really'})
+    }
+}
+
+module.exports = cors(handler);
