019ce74872a4c2e78a7238eac3741838d08b497b
raven <zlat.dominic.francisc@gmail.com>
2023-02-23 14:37:53 +0200

    var callback bug fix

--- api/dbop.js
+++ api/dbop.js
@@ -6,9 +6,13 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection(process.env.DB_KEY)
 
 function handler(req, res) {
-    connection.query('SELECT * FROM unn;', (err, res, fields) => {        
-        res.json({DAX: 'got it', RX: res})
-    });    
+    try{
+        connection.query('SELECT * FROM unn;', (err, resx, fields) => {        
+            res.json({DAX: 'got it', RX: resx})
+        });    
+    }catch(e){
+        res.json({DAX: 'got it', RX: `ERROR: ${e}`})
+    }
 }
 
 module.exports = cors(handler);
