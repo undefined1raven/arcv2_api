cdbedc63b9738d98ea7ac1963cf68830403ab857
raven <zlat.dominic.francisc@gmail.com>
2023-02-23 14:25:53 +0200

    create table

--- api/dbop.js
+++ api/dbop.js
@@ -3,8 +3,10 @@ const cors = require('micro-cors')({ allowMethods: ['GET', 'POST'], origin: '*' 
 const { v4 } = require('uuid');
 const bcrypt = require('bcrypt')
 const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 function handler(req, res) {
+    connection.execute('CREATE TABLE users(username varchar(255))');    
     if(process.env.PLANETSCALE_DB_USERNAME != undefined){
         res.json({DAX: 'got it'})
     }else{
