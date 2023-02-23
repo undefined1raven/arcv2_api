43ecdee6f7d6a1f35f31e96baed14abfb9728fbe
raven <zlat.dominic.francisc@gmail.com>
2023-02-23 15:38:12 +0200

    usage demo

--- api/dbop.js
+++ api/dbop.js
@@ -7,8 +7,12 @@ const connection = mysql.createConnection(process.env.DB_KEY)
 
 function handler(req, res) {
     try{
-        connection.query('SELECT * FROM unn', (err, resx, fields) => {        
-            res.json({DAX: 'got it', RX: resx})
+        connection.query('SELECT * FROM unn', (err, resx, fields) => {  
+            let s = 0;
+            for(let ix = 0; ix < resx.length; ix++){
+                s += resx[ix].uixx;
+            }      
+            res.json({DAX: 'got it', RX: resx, AVG: parseFloat(s / resx.length).toFixed(2)})
         });    
     }catch(e){
         res.json({DAX: 'got it', RX: `ERROR: ${e}`})
