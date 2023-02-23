357e40722c467f1ac1e0afb6488e8965d699bb81
raven <zlat.dominic.francisc@gmail.com>
2023-02-23 14:31:16 +0200

    i think i got it

--- api/dbop.js
+++ api/dbop.js
@@ -3,11 +3,11 @@ const cors = require('micro-cors')({ allowMethods: ['GET', 'POST'], origin: '*' 
 const { v4 } = require('uuid');
 const bcrypt = require('bcrypt')
 const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection(process.env.DB_KEY)
 
 function handler(req, res) {
-    connection.execute('CREATE TABLE users(username varchar(255))');    
-    if(process.env.PLANETSCALE_DB_USERNAME != undefined){
+    connection.query('CREATE TABLE users(username varchar(255));');    
+    if(process.env.DB_KEY != undefined){
         res.json({DAX: 'got it'})
     }else{
         res.json({DAX: 'not really'})
