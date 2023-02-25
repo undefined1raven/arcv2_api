1013bae02f0dd8c6035b039b6eae58a273a9d513
raven <zlat.dominic.francisc@gmail.com>
2023-02-25 12:23:22 +0200

    Auth Validation

--- api/auth.js
+++ api/auth.js
@@ -21,49 +21,58 @@ const db = admin.database();
 function handler(req, res) {
     console.log(req.query['val'])
     if (req.body != undefined && req.query['val'] != 0) {
-        let userid = req.body.userid;
-        let password = req.body.password;
-        let cip = req.body.ip;
-        let useridType = 'email';
-        if (userid.indexOf('@') == -1) {
-            useridType = 'username';
-        } else {
-            useridType = 'email';
-        }
-        let rowsActual = [];
-        let passRowsActual = [];
-        connection.query(`SELECT email FROM users WHERE ${useridType} = ?`, userid, function (err, rows, fields) { rowsActual = rows });
-        setTimeout(() => {
-            connection.query(`SELECT password FROM users WHERE ${useridType} = ?`, userid, function (err, rows, fields) { passRowsActual = rows });
-            if (rowsActual.length > 0) {
-                setTimeout(() => {
-                    bcrypt.compare(password, passRowsActual[0].password).then(auth_res => {
-                        if (auth_res) {
-                            let ntid = v4();
-                            const add_token_to_rtdb = ref(db, `authTokens/${ntid}`);
-                            set(add_token_to_rtdb, {
-                                tx: Date.now(),
-                                ip: cip,
-                                uidd: userid,
-                                uidt: useridType
-                            }).then(r => {
-                                res.json({ status: 'Successful', redirect: '/', AT: ntid })
-                            }).catch(e => {//ive no idea why but this catch is broken (gets exe even whenn set was successful)
-                                res.json({ status: 'Successful', redirect: '/', AT: ntid })
-                            });
-                        } else {
-                            res.json({ status: 'Auth Failed' });
-                        }
-                    });
-                }, 300);
+        if (req.query['val'] != 0) {
+            let userid = req.body.userid;
+            let password = req.body.password;
+            let cip = req.body.ip;
+            let useridType = 'email';
+            if (userid.indexOf('@') == -1) {
+                useridType = 'username';
             } else {
-                res.json({ status: 'Auth Failed' });
+                useridType = 'email';
             }
-        }, 300);
-    } else if(req.query['val'] != 0){
+            let rowsActual = [];
+            let passRowsActual = [];
+            connection.query(`SELECT email FROM users WHERE ${useridType} = ?`, userid, function (err, rows, fields) { rowsActual = rows });
+            setTimeout(() => {
+                connection.query(`SELECT password FROM users WHERE ${useridType} = ?`, userid, function (err, rows, fields) { passRowsActual = rows });
+                if (rowsActual.length > 0) {
+                    setTimeout(() => {
+                        bcrypt.compare(password, passRowsActual[0].password).then(auth_res => {
+                            if (auth_res) {
+                                let ntid = v4();
+                                const add_token_to_rtdb = ref(db, `authTokens/${ntid}`);
+                                set(add_token_to_rtdb, {
+                                    tx: Date.now(),
+                                    ip: cip,
+                                    uidd: userid,
+                                    uidt: useridType
+                                }).then(r => {
+                                    res.json({ status: 'Successful', redirect: '/', AT: ntid })
+                                }).catch(e => {//ive no idea why but this catch is broken (gets exe even whenn set was successful)
+                                    res.json({ status: 'Successful', redirect: '/', AT: ntid })
+                                });
+                            } else {
+                                res.json({ status: 'Auth Failed' });
+                            }
+                        });
+                    }, 300);
+                } else {
+                    res.json({ status: 'Auth Failed' });
+                }
+            }, 300);
+        } else if(req.query['val'] == 0) {
+            get(ref(db, `authTokens/${req.body.AT}`)).then(snap => {
+                const data = snap.val();
+                if(data != undefined && data.ip == req.body.CIP){
+                    res.json({ status: 'Validation Successful', flag: true });
+                }else{
+                    res.json({ status: 'Validation Failed', redirect: '/login' });
+                }
+            })
+        }
+    } else {
         res.json({ status: 'No Body Data' })
-    }else{
-        res.json({status: 'val'});
     }
 }
 
