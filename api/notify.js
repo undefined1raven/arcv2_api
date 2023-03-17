c214fc61220d11942827adcab0d8c8cf33aef966
raven <zlat.dominic.francisc@gmail.com>
2023-03-17 02:29:49 +0200

    Push Notification on new message debugging

--- api/notify.js
+++ api/notify.js
@@ -70,33 +70,57 @@ async function queryDB(queryStr) {
 }
 
 function handler(req, res) {
+    if (req.query['xxx'] != undefined) {
+        let notificationObj = {
+            app_id: process.env.ONESIG_ID,
+            title: { "en": "Ring Relay" },
+            web_buttons: [{ id: 'like', text: 'Like' }, { id: 'markAsSeen', text: 'Mark as seen' }],
+            contents: { "en": `New message from MCRN Command` },
+            channel_for_external_user_ids: 'push',
+            include_external_user_ids: ["d0b7e62c-6f27-4b2c-add9-6085d703c5a5"],
+            chrome_web_badge: "https://www.filepicker.io/api/file/proltSCwSWqb8QgZU0UD?filename=name.png",
+            icon: "https://www.filepicker.io/api/file/k8omnb4ySjCWXE0WQSw5?filename=name.png",
+        };
+        sendNotification(notificationObj, res);
+    } else {
+        if (req.body != undefined) {
+            if (req.body.AT && req.body.CIP) {
+                get(ref(db, `authTokens/${req.body.AT}`)).then(snap => {
+                    const data = snap.val();
+                    if (data != undefined && data.ip == req.body.CIP) {
+                        bcrypt.compare(`${req.body.AT}${process.env.AT_SALT}${req.body.CIP}`, data.hash).then(result => {
+                            if (result) {
+                                if (req.query['newMessageN'] != undefined) {
+                                    queryDB(`SELECT username FROM users WHERE uid='${req.body.targetUID}'`).then(resx => {
+                                        console.log(resx[0].username)
+                                        let notificationObj = {
+                                            app_id: process.env.ONESIG_ID,
+                                            title: { "en": "Ring Relay" },
+                                            web_buttons: [{ id: 'like', text: 'Like' }, { id: 'markAsSeen', text: 'Mark as seen' }],
+                                            contents: { "en": `New message from ${resx[0].username}` },
+                                            channel_for_external_user_ids: 'push',
+                                            include_external_user_ids: [data.said],
+                                            chrome_web_badge: "https://www.filepicker.io/api/file/proltSCwSWqb8QgZU0UD?filename=name.png",
+                                            icon: "https://www.filepicker.io/api/file/k8omnb4ySjCWXE0WQSw5?filename=name.png",
+                                        };
+                                        sendNotification(notificationObj, res);
 
-    if (req.body != undefined) {
-        get(ref(db, `authTokens/${req.body.AT}`)).then(snap => {
-            const data = snap.val();
-            if (data != undefined && data.ip == req.body.CIP) {
-                bcrypt.compare(`${req.body.AT}${process.env.AT_SALT}${req.body.CIP}`, data.hash).then(result => {
-                    if (result) {
-                        if (req.query['newMessage'] != undefined) {
-                            queryDB(`SELECT username FROM users WHERE uid='${req.body.targetUID}'`).then(resx => {
-                                let notificationObj = {
-                                    app_id: process.env.ONESIG_ID,
-                                    title: { "en": "Ring Relay" },
-                                    web_buttons: [{ id: 'like', text: 'Like' }, { id: 'markAsSeen', text: 'Mark as seen' }],
-                                    contents: { "en": `New message from ${resx[0].username}` },
-                                    channel_for_external_user_ids: 'push',
-                                    include_external_user_ids: [data.said],
-                                    chrome_web_badge: "https://www.filepicker.io/api/file/proltSCwSWqb8QgZU0UD?filename=name.png",
-                                    icon: "https://www.filepicker.io/api/file/k8omnb4ySjCWXE0WQSw5?filename=name.png",
-                                };
-                                sendNotification(notificationObj, res);
-
-                            }).catch(e => res.json({ status: 'Failed', error: e, id: 'MSG-NOTF-24' }))
-                        }
+                                    }).catch(e => res.json({ status: 'Failed', error: e, id: 'MSG-NOTF-24' }))
+                                } else {
+                                    res.json({ status: 'Denied', id: 'NO-OP' })
+                                }
+                            } else {
+                                res.json({ status: 'Denied', id: 'H2' })
+                            }
+                        })
+                    } else {
+                        res.json({ status: 'Denied', id: 'L8' })
                     }
-                })
+                }).catch(e => res.json({ status: 'Failed', error: e, id: 'Auth-24' }))
+            } else {
+                res.json({ status: 'Denied' })
             }
-        })
+        }
     }
 }
 
